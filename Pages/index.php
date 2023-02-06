83ad2f027c13f295eef3073fb700cd1a10a822e4
Rentler Ferenc <rentler.feri@gmail.com>
2023-02-06 15:50:30 +0100

    footer social incons added.

--- Pages/index.php
+++ Pages/index.php
@@ -6,6 +6,7 @@
     <link href="../style/menu.css" rel="stylesheet" type="text/css" />
     <link href="../style/registration.css" rel="stylesheet" type="text/css" />
     <link href="../style/footer.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
 
 </head>
@@ -56,7 +57,26 @@
     </div>
 </body>
 <footer>
-  <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> Rentler Ferenc </p>
+  <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script></p>
+  <p float= "right"> My github is<a href="https://github.com/rentlerferi/Szakdolgozat.git" target="https://github.com/rentlerferi/Szakdolgozat.git" >here.</a> </p>
+  <p class="cofee"> Buy me a <a href="" >coffe</a>. </p>
+  <div class="social">
+	<ul>
+		 <li>
+			<a href="https://uni-pannon.hu/hu/" target="https://uni-pannon.hu/hu/" title="Egyetemi weboldal"><i class="fa fa-globe"></i></a> 
+ 		 </li>
+		  <li>
+			<a href="https://www.facebook.com/UniPannon/?locale=hu_HU" target="https://www.facebook.com/UniPannon/?locale=hu_HU" title="Hivatalos facebook"><i class="fa fa-facebook"></i></a> 
+ 		 </li>
+		  <li>
+			<a href="https://www.youtube.com/user/egyetemtv" target="https://www.youtube.com/user/egyetemtv" title="Hivatalos youtube"><i class="fa fa-youtube"></i></a> 
+ 		 </li>
+		  <li>
+			<a href="https://www.instagram.com/pannon_egyetem/?hl=hu" target="https://www.instagram.com/pannon_egyetem/?hl=hu" title="Instagram"><i class="fa fa-instagram"></i></a> 
+ 		 </li>
+	</ul>
+  </div>
+
 </footer>
 
 
